079f00844dfa3f2560d80f9bf2eef031eacc314e
Nero Li <ljp2745559@gmail.com>
2015-10-18 00:55:50 -0700

    fix a bug in communicator

--- threads/Communicator.java
+++ threads/Communicator.java
@@ -38,13 +38,13 @@ public class Communicator {
 	 */
 	public void speak(int word) {
         lock.acquire();
-        //System.out.println("speaking sleeps at: " + Machine.timer().getTime());
+        System.out.println("speaking sleeps at: " + Machine.timer().getTime());
         //speak_lock.sleep();
         while(listenning <= 0)
             speak_lock.sleep();
         text.add(word);
         //textbuffer = word;
-        //System.out.println("text added is: " + text.peek());
+        System.out.println("text added is: " + text.peek());
         listen_lock.wake();
         lock.release();
 	}
@@ -58,15 +58,16 @@ public class Communicator {
 	public int listen() {
         
         lock.acquire();
-        //System.out.println("speaking wakes at: " + Machine.timer().getTime());
+        System.out.println("speaking wakes at: " + Machine.timer().getTime());
         //speak_lock.wake();
         listenning++;
+        speak_lock.wake();
         int returned = 0;
         while(text.isEmpty())
             listen_lock.sleep();
         returned = text.removeFirst();
         //returned = textbuffer;
-        //System.out.println("returned text is: " + returned);
+        System.out.println("returned text is: " + returned);
         lock.release();
         return returned;
 	}
@@ -83,54 +84,55 @@ public class Communicator {
     
 
 	public static void selfTest(){
-    final Communicator com = new Communicator();
-    final long times[] = new long[4];
-    final int words[] = new int[2];
-    KThread speaker1 = new KThread( new Runnable () {
-        public void run() {
-            com.speak(4);
-            times[0] = Machine.timer().getTime();
-            //System.out.println("time0 is: " + times[0]);
-        }
-    });
-    speaker1.setName("S1");
-    KThread speaker2 = new KThread( new Runnable () {
-        public void run() {
-            com.speak(7);
-            times[1] = Machine.timer().getTime();
-            //System.out.println("time1 is: " + times[1]);
-        }
-    });
-    speaker2.setName("S2");
-    KThread listener1 = new KThread( new Runnable () {
-        public void run() {
-            words[0] = com.listen();
-             //System.out.println("words0 is: " + words[0]);
-            times[2] = Machine.timer().getTime();
-            //System.out.println("time2 is: " + times[2]);
-        }
-    });
-    listener1.setName("L1");
-    KThread listener2 = new KThread( new Runnable () {
-        public void run() {
-            words[1] = com.listen();
-          // System.out.println("words1 is: " + words[1]);
-            times[3] = Machine.timer().getTime();
-            //System.out.println("time3 is: " + times[3]);
-        }
-    });
-    listener2.setName("L2");
-    
-    //listener1.fork(); speaker2.fork(); speaker1.fork(); listener2.fork();
-    //speaker1.join(); speaker2.join(); listener1.join(); listener2.join();
-    //listener1.fork();
-    //listener1.join();
-    //speaker1.fork();
-    //speaker1.join();
-    
-    Lib.assertTrue(words[0] == 4, "Didn't listen back spoken word."); 
-    Lib.assertTrue(words[1] == 7, "Didn't listen back spoken word.");
-    Lib.assertTrue(times[0] < times[2], "speak returned before listen.");
-    Lib.assertTrue(times[1] < times[3], "speak returned before listen.");
-}
+        final Communicator com = new Communicator();
+        final long times[] = new long[4];
+        final int words[] = new int[2];
+        KThread speaker1 = new KThread( new Runnable () {
+            public void run() {
+                com.speak(4);
+                times[0] = Machine.timer().getTime();
+                System.out.println("time0 is: " + times[0]);
+            }
+        });
+        speaker1.setName("S1");
+        KThread speaker2 = new KThread( new Runnable () {
+            public void run() {
+                com.speak(7);
+                times[1] = Machine.timer().getTime();
+                System.out.println("time1 is: " + times[1]);
+            }
+        });
+        speaker2.setName("S2");
+        KThread listener1 = new KThread( new Runnable () {
+            public void run() {
+                words[0] = com.listen();
+                System.out.println("words0 is: " + words[0]);
+                times[2] = Machine.timer().getTime();
+                System.out.println("time2 is: " + times[2]);
+            }
+        });
+        listener1.setName("L1");
+        KThread listener2 = new KThread( new Runnable () {
+            public void run() {
+                words[1] = com.listen();
+                System.out.println("words1 is: " + words[1]);
+                times[3] = Machine.timer().getTime();
+                System.out.println("time3 is: " + times[3]);
+            }
+        });
+        listener2.setName("L2");
+        
+        listener1.fork(); listener2.fork(); speaker1.fork(); speaker2.fork();
+        listener1.join(); speaker2.join(); speaker1.join(); listener2.join();
+       // speaker1.fork();
+        //listener1.fork();
+        
+        //listener1.join();
+        //speaker1.join();
+        
+        Lib.assertTrue(words[0] == 4, "Didn't listen back spoken word."); 
+        //Lib.assertTrue(words[1] == 7, "Didn't listen back spoken word.");
+        Lib.assertTrue(times[0] < times[2], "speak returned before listen.");
+        //Lib.assertTrue(times[1] < times[3], "speak returned before listen.");
+    }
 }
